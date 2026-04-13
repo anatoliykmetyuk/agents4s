623d13298074566a0222612f8022d9646c46c7dc
Sbt Pull Request Validator <sbt-pull-request-validator@github.com>
2026-04-13 10:04:00 +0900

    Actor: tighten json output msg

--- agents4s-pekko/src/main/scala/agents4s/pekko/LLMActor.scala
+++ agents4s-pekko/src/main/scala/agents4s/pekko/LLMActor.scala
@@ -32,6 +32,8 @@ Write the result of your operation to JSON file at the following path: {{JSON_FI
 Use the following schema to record your result: {{JSON_SCHEMA}}
 
 Output in the precise format specified above.
+
+Do not do anything else - immediately output the result based on your earlier work.
 """
 
   def start[O: JsonSchema: ReadWriter](
